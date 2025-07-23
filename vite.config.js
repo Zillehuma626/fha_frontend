29fa7d394916eb66ab85f15ffdc963af8cd34586
Zillehuma626 <zille626@gmail.com>
2025-07-23 23:59:26 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: 'fha-accountants.onrender.com'
+  },
   assetsInclude:['**/*.PNG'],
   server:{
     host:true,
