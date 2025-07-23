6867e2072b8c2d7cfb9d9b6f620e93fad294ac68
Zillehuma626 <zille626@gmail.com>
2025-07-23 20:43:57 +0500

    Update server.js

--- fha-backend/server.js
+++ fha-backend/server.js
@@ -6,7 +6,7 @@ const contactRoutes = require('./routes/contact');
 
 dotenv.config();
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
@@ -18,8 +18,8 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => {
   console.log('✅ Connected to MongoDB Atlas');
-  app.listen(process.env.PORT, () => {
-    console.log(`🚀 Server running on port ${process.env.PORT}`);
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
   });
 })
 .catch((error) => console.error('MongoDB connection error:', error));
