fdcf96f3c3e59f1d78bfcb060cfad01cfd70e312
Zillehuma626 <zille626@gmail.com>
2025-07-23 22:24:39 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude:['**/*.PNG'],
   server:{
     host:true,
     strictPort:true,
     port:8000
   },
-  resolve:{
-  assetsInclude:['**/*.PNG'],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 });
