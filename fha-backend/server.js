950184229eccdf69b02296237e11e675b635a8ab
Zillehuma626 <zille626@gmail.com>
2025-07-23 23:40:47 +0500

    Update server.js

--- fha-backend/server.js
+++ fha-backend/server.js
@@ -6,7 +6,7 @@ const contactRoutes = require('./routes/contact');
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
