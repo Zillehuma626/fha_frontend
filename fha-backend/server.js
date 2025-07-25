8aa8c53faf81c3e3dc63e9841b9e1a4ffaa0c2ec
Zillehuma626 <zille626@gmail.com>
2025-07-25 18:52:45 +0500

    Update server.js

--- fha-backend/server.js
+++ fha-backend/server.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const contactRoutes = require('./routes/contact');
+const getMyCodeRoutes = require('./routes/getMyCode'); // ✅ new
 
 dotenv.config();
 const app = express();
-const PORT = PORT || 8000;
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(cors());
 app.use('/api/contact', contactRoutes);
+app.use('/api/get-my-code', getMyCodeRoutes); // ✅ new
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
