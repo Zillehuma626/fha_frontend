2a24c1c910ba9b1d7da020ec7ae28267a019042c
Zillehuma626 <zille626@gmail.com>
2025-07-26 02:56:34 +0500

    Update server.js

--- fha-backend/server.js
+++ fha-backend/server.js
@@ -31,7 +31,9 @@ app.use(cors({
 
 app.use('/api/contact', contactRoutes);
 app.use('/api/get-my-code', getMyCodeRoutes); // ✅ new
-
+app.get("/", (req, res) => {
+  res.send("✅ FHA Backend is running.");
+});
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
